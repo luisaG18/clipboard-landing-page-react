ab7371eefc534eca28e16ff2a9579111f0c1ef19
Luisa Giraldo <luiisagiraldori@gmail.com>
2023-10-18 15:44:53 -0500

    Create button component

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.scss";
 import ImageHeader from "../../assets/images/bg-header-mobile.png";
 import Logo from "../../assets/images/logo.svg";
+import Button from "../../components/Button/Button";
 
 function Home() {
   return (
@@ -16,8 +17,16 @@ function Home() {
             Clipboard allows you to track and organize everything you copy.
             Instantly access your clipboard on all your devices.
           </span>
-          <button>Download for IOS</button>
-          <button>Download for Mac</button>
+          <Button
+            text="Download for IOS"
+            backgroundColor="#31b9a4"
+            boxshadow="4px 4px 5px rgb(41, 147, 128)"
+          />
+          <Button
+            text="Download for Mac"
+            backgroundColor="#5F78FA"
+            boxshadow="4px 4px 5px rgb(87, 102, 179)"
+          />
         </section>
         <footer>info</footer>
       </div>
